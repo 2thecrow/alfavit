1915ff2e8a78bbe224cb8a51270abfbbaaad3a01
Alexander Pletnev <pletnev.a.a@yandex.ru>
2021-04-12 18:31:58 +0300

    Сделал интерактивную карту. Начал слайдер

--- app/js/_tmp/userscripts.tmp.js
+++ app/js/_tmp/userscripts.tmp.js
@@ -2,4 +2,23 @@
 
 document.addEventListener('DOMContentLoaded', function () {
   AOS.init();
+  var ApartSlider = new Swiper('.apartment-slider', {
+    // Optional parameters
+    loop: true,
+    slidesPerView: 1,
+    spaceBetween: 30,
+    // If we need pagination
+    pagination: {
+      el: '.swiper-pagination'
+    },
+    // Navigation arrows
+    navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev'
+    },
+    // And if we need scrollbar
+    scrollbar: {
+      el: '.swiper-scrollbar'
+    }
+  });
 });
--- app/js/app.js
+++ app/js/app.js
@@ -2,4 +2,27 @@ document.addEventListener('DOMContentLoaded', () => {
 
     AOS.init();
 
+    let ApartSlider = new Swiper('.apartment-slider', {
+        // Optional parameters
+        loop: true,
+        slidesPerView: 1,
+        spaceBetween: 30,
+      
+        // If we need pagination
+        pagination: {
+          el: '.swiper-pagination',
+        },
+      
+        // Navigation arrows
+        navigation: {
+          nextEl: '.swiper-button-next',
+          prevEl: '.swiper-button-prev',
+        },
+      
+        // And if we need scrollbar
+        scrollbar: {
+          el: '.swiper-scrollbar',
+        },
+      });
+
 })
